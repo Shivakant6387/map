e0dd6faa9efb7becaf4603bec958230b2c0da42c
Shivakant6387 <shivakant5875@gmail.com>
2023-01-30 12:01:20 +0530

    comparing by value

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Integer , String>map=new HashMap<>();
+        map.put(100,"Jackson");
+        map.put(101,"Ethan");
+        map.put(102,"Logan");
+        map.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(System.out::println);
     }
 }
